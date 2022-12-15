69adf6e9d58f8b52de4542328b18aa0edd2cce1b
Arsene Claudiu Ion <arseneclaudiuion@192-168-0-179.rdsnet.ro>
2022-12-15 09:37:44 +0200

    refactoring

--- src/js/hierarchical-taxonomy-tree.js
+++ src/js/hierarchical-taxonomy-tree.js
@@ -3,34 +3,38 @@
  */
 
 (function ($, Drupal, drupalSettings) {
-  'use strict';
-  $(document).ready(function () {
-    $('.menu-item.menu-item--expanded').each(function (i, obj) {
-      let self = $(this);
-      if (self.find('a.active').length) {
-        self.addClass('active');
 
-        if (drupalSettings.interactiveParentMenu) {
-          if (!self.hasClass('menu-item--active')) {
-            self.children('i').toggleClass('arrow-right arrow-down');
+  Drupal.behaviors.hierarhical_tax_tree = {
+    attach: function (context, settings) {
+      once('hierarhical_tax_tree', '.helfi_ahjo--expanded', context).forEach(function (i, obj) {
+        let self = $(this);
+
+        if (self.find('a.active').length) {
+          self.addClass('active');
+
+          if (drupalSettings.interactiveParentMenu) {
+            if (!self.hasClass('active')) {
+              self.children('i').toggleClass('arrow-right arrow-down');
+            }
           }
         }
-      }
-    });
+      });
 
-    $('.hierarchical-taxonomy-tree .menu-item--expanded > a').on('click', function (e) {
-      e.preventDefault();
-      $(this).find('i').toggleClass('arrow-right arrow-down');
-      let isChildVisible = $(this).parent().children('.menu').is(':visible');
-      if (isChildVisible) {
-        $(this).parent().children('.menu').slideUp();
-        $(this).parent().removeClass('active');
-      }
-      else {
-        $(this).parent().children('.menu').slideDown();
-        $(this).parent().addClass('active');
-      }
-    });
+      $('.hierarchical-taxonomy-tree .helfi_ahjo--expanded > a').on('click', function (e) {
+        e.preventDefault();
+        $(this).find('i').toggleClass('arrow-right arrow-down');
+        let isChildVisible = $(this).parent().children('.ahjo-tree').is(':visible');
+        if (isChildVisible) {
+          $(this).parent().children('.ahjo-tree').slideUp();
+          $(this).parent().removeClass('active');
+        }
+        else {
+          $(this).parent().children('.ahjo-tree').slideDown();
+          $(this).parent().addClass('active');
+        }
+      });
 
-  });
+    }
+  };
+
 })(jQuery, Drupal, drupalSettings);
